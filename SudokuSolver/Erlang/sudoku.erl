afc7df2173cce94e731c571b6b45982c6e918233
vicsuka <vicsuka@gmail.com>
2019-11-23 16:10:12 +0100

    Solutions done in different arrays

--- SudokuSolver/Erlang/sudoku.erl
+++ SudokuSolver/Erlang/sudoku.erl
@@ -1,7 +1,7 @@
 -module(sudoku).
 -author('vicsuka@gmail.com').
 -vsn('2019-11-17').
--export([sudoku/1,calculatePossibilites/6,addFirstPossibleValue/7,addPossibleSteps/3,removeDoubleBrackets/2]).
+-export([sudoku/1,calculatePossibilites/6,addFirstPossibleValue/7,addPossibleSteps/3,removeDoubleBrackets/2,multiplyList/4]).
 
 -import(khf1,[feldarabolasa/2]).
 -import(khf2,[ertekek/2]).
@@ -25,7 +25,7 @@ sudoku({_,Matrix}) -> solveProblems(Matrix, []).
 
 solveProblems(Matrix, []) ->
     Fullsize = length(Matrix),
-    ReSolution = solveSudokuV2(Fullsize,Matrix,[],[],1,[]),
+    ReSolution = solveSudokuV3(Fullsize,Matrix,[],[],1,[]),
     ReSolution.
 
 solveSudokuV2(Fullsize,Matrix,[],[],Counter,[]) ->
@@ -168,6 +168,192 @@ solveSudokuV2(Fullsize,Matrix,PossibleSteps,_,Counter,SolutionsArray,BackUpArray
         
         end
         
+    end.
+
+solveSudokuV3(Fullsize,Matrix,[],[],Counter,[]) ->
+    SimpleMatrixList = feldarabolasa(Matrix,{1,1}),
+    PossibleSteps = calculatePossibilites(isqrt(Fullsize),Matrix,SimpleMatrixList,1,1,[]),
+    % SimpleList = addPossibleSteps(SimpleMatrixList, [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],[]),
+    % %io:format("Possible: ~p ~n",[PossibleSteps]),
+
+    solveSudokuV3(Fullsize,Matrix,PossibleSteps,SimpleMatrixList,Counter,[]);
+
+
+solveSudokuV3(Fullsize,Matrix,PossibleSteps,_,Counter,SolutionsArray) ->    
+    if (Counter > 20) ->
+        io:format("COULD NOT SOLVE ~n");
+    (1==1) ->           
+        SimpleMatrixList = removeDoubleBrackets(feldarabolasa(Matrix,{1,1}),[]),
+        io:format("STEP: ~p ~n",[Counter]),
+        io:format("FOR MATRIX: ~p ~n",[Matrix]),
+        io:format("Possible: ~p ~n",[PossibleSteps]),
+        io:format("List: ~p ~n",[SimpleMatrixList]),
+
+        %ADD VALUES THAT HAVE ONLY 1 POSSIBILITY
+        NextSimpleList = addPossibleSteps(SimpleMatrixList,PossibleSteps,[]),
+        io:format("SURE VALUES: ~p ~n",[NextSimpleList]),
+
+        if (NextSimpleList == SimpleMatrixList) ->
+
+            %FIND BEST FIELD (SMALLEST POSSIBLE STEPS BIGGER THAN 1)
+            Smallest = findSmallest(PossibleSteps,16),
+            %io:format("Smallest: ~p ~n",[Smallest]),
+            if (Smallest > 15) ->
+                {Fullsize,Matrix};
+            (1==1) ->
+
+
+                %ADD FIRST POSSIBLE VALUE TO MX
+                ToReplaceList = tryDifferentValues(SimpleMatrixList, PossibleSteps, [], Smallest),
+                
+                %io:format("WE CHOSE: ~p, BACKUPS: ~p at: ~p ~n",[AdvancedSimpleList,NewBackUpArray,NewBackUpIndex]),
+                AllPossibleMatrixes = multiplyList(ToReplaceList,Smallest,1,[]),
+                BranchingArray = getBranch(PossibleSteps,Smallest),
+                
+                AdvancedSimpleLists = forAllList(AllPossibleMatrixes, BranchingArray, []),
+                io:format("AdvancedSimpleLists: ~p ~n",[AdvancedSimpleLists]),
+
+
+                Results = doBranching(Fullsize,AdvancedSimpleLists,[],Counter),
+                Results;
+            
+        (1==1) ->
+            io:format("ADDED VALUE%!!!% ~n"),
+            %PARTITION CURRENT CONSTRAINT TO MATRIX
+            NextMatrix = createPartition(NextSimpleList,Fullsize,[],[],1),
+            %io:format("NextMatrix: ~p ~n",[NextMatrix]),
+
+            %NEXT POSSIBLE MOVES
+            NextPossibleSteps = calculatePossibilites(isqrt(Fullsize),NextMatrix,NextSimpleList,1,1,[]),
+            %io:format("NextPossibleSteps: ~p ~n",[NextPossibleSteps]),
+
+            %CURRENT SOLUTION FOR CHECKING
+            ThisSolution = addToSolution(isqrt(Fullsize),NextPossibleSteps,[],0),
+            PartitionedSolution = createPartition(ThisSolution,Fullsize,[],[],1),
+            %io:format("PartitionedSolution: ~p ~n",[PartitionedSolution]),
+
+            %LOGGING
+            io:format("Current SOLUTION: ~p ~n",[PartitionedSolution]),
+
+            %CHECK IF SUDOKU IS SOLVEABLE
+            Possible = checkPossibility(NextPossibleSteps),
+            %io:format("Possible? ~p ~n",[Possible]),
+
+            IsDone = megoldase({isqrt(Fullsize),NextMatrix},PartitionedSolution),
+            io:format("DONE? ~p ~n",[IsDone]),
+
+            if (IsDone) ->
+                PartitionedSolution;
+            (1==1) ->
+                if (Possible) ->
+                    solveSudokuV3(Fullsize,NextMatrix,NextPossibleSteps,NextSimpleList,Counter+1,SolutionsArray);
+                (1==1) ->                
+                    {Fullsize,Matrix}
+                end
+            end
+        
+        end
+        
+    end.
+
+doBranching(_,[],Results,_) -> Results;
+doBranching(Fullsize,[CurrentList|NextList], Results,Counter) ->
+    %PARTITION CURRENT CONSTRAINT TO MATRIX
+    NextMatrix = createPartition(CurrentList,Fullsize,[],[],1),
+    %io:format("NextMatrix: ~p ~n",[NextMatrix]),
+
+    %NEXT POSSIBLE MOVES
+    NextPossibleSteps = calculatePossibilites(isqrt(Fullsize),NextMatrix,CurrentList,1,1,[]),
+    %io:format("NextPossibleSteps: ~p ~n",[NextPossibleSteps]),
+
+    %CURRENT SOLUTION FOR CHECKING
+    ThisSolution = addToSolution(isqrt(Fullsize),NextPossibleSteps,[],0),
+    PartitionedSolution = createPartition(ThisSolution,Fullsize,[],[],1),
+    %io:format("PartitionedSolution: ~p ~n",[PartitionedSolution]),
+
+    %LOGGING
+    %io:format("Current SOLUTION: ~p ~n",[PartitionedSolution]),
+
+    %CHECK IF SUDOKU IS SOLVEABLE
+    Possible = checkPossibility(NextPossibleSteps),
+
+    IsDone = megoldase({isqrt(Fullsize),NextMatrix},PartitionedSolution),
+    %io:format("DONE? ~p ~n",[IsDone]),
+
+    if (IsDone) ->
+        PartitionedSolution;
+    (1==1) ->
+        if (Possible) ->
+            Solve = solveSudokuV3(Fullsize,NextMatrix,NextPossibleSteps,Results,Counter+1,Results),
+            if (is_list(Solve)) ->
+                NewResults = Results ++ [Solve],
+                doBranching(Fullsize, NextList, NewResults,Counter);  
+            (1==1) ->
+                doBranching(Fullsize, NextList, Results,Counter)
+            end;
+        (1==1) ->                
+            {Fullsize,NextMatrix}
+        end
+    end.
+
+multiplyList([], _, _, FullList) -> FullList;
+multiplyList([H|T], Size, Counter, FullList) ->
+    if (Size < Counter) ->
+        multiplyList([],Size,Counter,FullList);
+    (1==1) ->
+        NewRes = FullList ++ [[H|T]],
+        multiplyList([H|T], Size, Counter+1, NewRes)
+    end.
+
+getBranch([PossH|PossT],Smallest) ->
+    PossibNumber = length(PossH),
+    if (PossibNumber == Smallest) ->
+        PossH;
+    (1==1) ->
+        getBranch(PossT, Smallest)
+    end.
+
+forAllList([],[],Result) -> Result;
+forAllList([],_,Result) -> Result;
+forAllList(_,[],Result) -> Result;
+forAllList([H|T], [VH|VT], Result) ->
+    NewVal = Result ++ [replaceValues(H,VH,[])],
+    forAllList(T,VT,NewVal).
+
+replaceValues([],[],Result) -> Result;
+replaceValues([],_,Result) -> Result;
+replaceValues(_,[],Result) -> Result;
+replaceValues([SolH|SolT], Value, Result) ->
+    Contained = listFind(replace,SolH),
+    if (Contained) ->
+        %Remove last element that is 'replace'
+        Appended = append(lists:reverse(tl(lists:reverse(SolH))),[Value]),
+        NewRes = Result ++ [Appended],
+        replaceValues(SolT,Value,NewRes);
+    (1==1) ->
+        NewRes = Result ++ [SolH],
+        replaceValues(SolT,Value,NewRes)
+    end.
+
+tryDifferentValues([],[],Result, _) -> Result;
+tryDifferentValues([],_,Result, _) -> Result;
+tryDifferentValues(_,[],Result, _) -> Result;
+tryDifferentValues([SolH|SolT], [PossH|PossT], Result, Smallest) ->
+    PossibNumber = length(PossH),
+    if (PossibNumber == Smallest) ->
+            [TheValue|_] = PossH,
+            AlreadyContained = listFind(TheValue,SolH),
+            if (AlreadyContained) ->
+                NewRes = Result ++ [SolH],
+                tryDifferentValues(SolT,PossT,NewRes, Smallest);
+            (1==1) ->
+                Appended = append(SolH,[replace]),
+                NewRes = Result ++ [Appended],
+                tryDifferentValues(SolT,PossT,NewRes, 999)
+            end;
+    (1==1) ->
+        NewRes = Result ++ [SolH],
+        tryDifferentValues(SolT,PossT,NewRes, Smallest)
     end.
 
 removeDoubleBrackets([],Result) ->Result;
